474501f5c17319e02901c2ac0bb57074e3c4c55c
inh556 <yanbin.jin@gmail.com>
2018-10-25 20:37:29 -0700

    refactored index view to use database API get 5 latest questions

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from .modeles import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_questions_list = Question.objects.order_by('-pub_date')[:5];
+    output = ', '.join([q.question_text for q in latest_questions_list]);
+    return HttpResponse("output");
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
